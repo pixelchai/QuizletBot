d4799f0e4c4313b56597f821a1e25c04382970ba
pixelzerg <abhinavbhandari4@gmail.com>
2015-12-13 13:42:06 -0500

    Clean Up

--- SpaceRaceBot.js
+++ SpaceRaceBot.js
@@ -10,37 +10,27 @@
 /* jshint -W097 */
 /*eslint-env browser, jquery*/
 'use strict';
-//var quizbotrunning=true;
 var answers = eval((/QTerm.dataToArray\(.*\)/g).exec($('html').html())[0]);
 //terms;
 $(".value").keydown(function()
 {
-    if ($(".value").length > 0)
-    {
-        //	console.log("Translating:"+$(".free").first().children().text());
-        $(".value").val(toeng($(".free").first().children().text()));
-    }
-
-    function executeAsync(func)
-    {
-        setTimeout(func, 0);
-    }
+	if ($(".value").length > 0)
+	{
+		$(".value").val(toeng($(".free").first().children().text()));
+	}
 });
 
 function toeng(s)
 {
-    var no = 0;
-    while (no !== answers.length)
-    {
-        console.log("comparing: " + answers[no].definition + " and " + s);
-        if (s == answers[no].definition)
-        {
-            console.log("Translation got: " + answers[no].word);
-            //		return answers[no+1];
-            return answers[no].word;
-        }
-        no++;
-    }
-    console.error("mooo!");
-    return null;
+	var no = 0;
+	while (no !== answers.length)
+	{
+		if (s == answers[no].definition)
+		{
+			return answers[no].word;
+		}
+		no++;
+	}
+	console.error("mooo!");
+	return null;
 }
