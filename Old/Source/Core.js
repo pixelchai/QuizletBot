1a7d581973ea1e8f565ae501ebe8ffc2f6362c61
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-11-16 20:50:01 +0000

    moved old files to old

---
+++ Old/Source/Core.js
@@ -0,0 +1,11 @@
+/*eslint-env jquery, browser*/
+var termsrgx = /{"terms":\[[^\]]+\]}/;
+
+function parse(code){
+	$.ajax({
+    url : "https://quizlet.com/"+code+"/export",
+    success : function(result){
+    	return JSON.parse(result.match(termsrgx));
+    }
+});
+}
